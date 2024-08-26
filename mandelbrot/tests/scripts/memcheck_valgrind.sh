76a8fac5f223893cee49c6e9ac1b0d8f6a4479f0
tunashred <enachealexandru.traian@gmail.com>
2024-08-26 19:35:19 +0300

    Change the log save location to tests/output directory

--- mandelbrot/tests/scripts/memcheck_valgrind.sh
+++ mandelbrot/tests/scripts/memcheck_valgrind.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: ./memcheck_valgrind <executable_path>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./memcheck_valgrind <executable_path> <log_save_path>"
     exit 1
 fi
 
 EXECUTABLE_PATH=$1
+LOG_SAVE_PATH=$2
 
-valgrind --tool=memcheck --leak-check=full --log-file=valgrind_memcheck.log ${EXECUTABLE_PATH}
+valgrind --tool=memcheck --leak-check=full --log-file="$LOG_SAVE_PATH/valgrind_memcheck.log" "$EXECUTABLE_PATH"
 
-tail -n 20 valgrind_memcheck.log
+tail -n 20 "$LOG_SAVE_PATH/valgrind_memcheck.log"
