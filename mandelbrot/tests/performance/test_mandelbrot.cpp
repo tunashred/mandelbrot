a9c00a1d6ce9664a4ecba1b61410e2361b5b00f9
tunashred <enachealexandru.traian@gmail.com>
2024-09-08 22:37:24 +0300

    Update tests

--- mandelbrot/tests/performance/test_mandelbrot.cpp
+++ mandelbrot/tests/performance/test_mandelbrot.cpp
@@ -20,179 +20,179 @@ class Environment : public ::testing::Environment {
 // Mandelbrot
 TEST(image_generation, mandelbrot_1k) {
     float scale = 1;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, mandelbrot_1_5k) {
     float scale = 1.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation, mandelbrot_2k) {
     float scale = 2;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, mandelbrot_2_5k) {
     float scale = 2.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, mandelbrot_3k) {
     float scale = 3;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, mandelbrot_3_5k) {
     float scale = 3.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation, mandelbrot_4k) {
     float scale = 4;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 // Galaxy
 TEST(image_generation, galaxy_1k) {
     float scale = 1;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, galaxy_1_5k) {
     float scale = 1.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation, galaxy_2k) {
     float scale = 2;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, galaxy_2_5k) {
     float scale = 2.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, galaxy_3k) {
     float scale = 3;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, galaxy_3_5k) {
     float scale = 3.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation, galaxy_4k) {
     float scale = 4;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 // Stuff
 TEST(image_generation, stuff_1k) {
     float scale = 1;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, stuff_1_5k) {
     float scale = 1.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation, stuff_2k) {
     float scale = 2;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, stuff_2_5k) {
     float scale = 2.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, stuff_3k) {
     float scale = 3;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, stuff_3_5k) {
     float scale = 3.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation, stuff_4k) {
     float scale = 4;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 // Seastar
 TEST(image_generation, seastar_1k) {
     float scale = 1;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, seastar_1_5k) {
     float scale = 1.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation, seastar_2k) {
     float scale = 2;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, seastar_2_5k) {
     float scale = 2.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, seastar_3k) {
     float scale = 3;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, seastar_3_5k) {
     float scale = 3.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation, seastar_4k) {
     float scale = 4;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 // Shells
 TEST(image_generation, shells_1k) {
     float scale = 1;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, shells_1_5k) {
     float scale = 1.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation, shells_2k) {
     float scale = 2;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, shells_2_5k) {
     float scale = 2.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, shells_3k) {
     float scale = 3;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation_extra, shells_3_5k) {
     float scale = 3.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
 
 TEST(image_generation, shells_4k) {
     float scale = 4;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2);
+    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
 }
