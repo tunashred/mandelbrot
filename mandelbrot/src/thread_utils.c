10f56b32d01593cf16b1380a5e4960cbce98a8ec
tunashred <enachealexandru.traian@gmail.com>
2024-09-09 16:39:33 +0300

    Fix memory leak for workers_info

--- mandelbrot/src/thread_utils.c
+++ mandelbrot/src/thread_utils.c
@@ -3,13 +3,14 @@
 #include "thread_utils.h"
 #include "mandelbrot.h"
 
-static pthread_t* threads = NULL;
 static uint64_t g_thread_count = 0; //TODO: revert this back to an int and just explicitly convert when needed
+static pthread_t* threads = NULL;
+static worker_task_info* workers_info = NULL;
 
 void start_worker_threads(const uint64_t* thread_count, color_palette* palette, image_info* image_info, int** buffer) {
     threads = (pthread_t*) malloc(*thread_count * sizeof(pthread_t));
     g_thread_count = *thread_count;
-    worker_task_info* workers_info = (worker_task_info*) malloc(*thread_count * sizeof(worker_task_info)); //TODO: investigate sizeof(workers_info) case
+    workers_info = (worker_task_info*) malloc(*thread_count * sizeof(worker_task_info)); //TODO: investigate sizeof(workers_info) case
 
     const int slice_height = *(image_info->height) / *thread_count;
 
@@ -35,7 +36,10 @@ void wait_all_threads() {
     for(uint64_t i = 0; i < g_thread_count; i++) {
         pthread_join(threads[i], NULL);
     }
+    g_thread_count = 0;
     free(threads);
     threads = NULL;
-    g_thread_count = 0;
+
+    free(workers_info);
+    workers_info = NULL;
 }
