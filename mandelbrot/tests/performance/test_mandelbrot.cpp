f26bd92938d2abddaa6db87275cc5dbaa0a1bae4
tunashred <enachealexandru.traian@gmail.com>
2024-09-13 14:50:54 +0300

    Reorganized tests

--- mandelbrot/tests/performance/test_mandelbrot.cpp
+++ mandelbrot/tests/performance/test_mandelbrot.cpp
@@ -1,198 +1,122 @@
+#include "gtest/gtest.h"
 #include <mandelbrot.h>
 #include <color_mapping.h>
 
-#include <gtest/gtest.h>
-
-// TODO: Reuse same image for overwritting + change its save location + sanitizer integration
+// TODO: Reuse same image for overwriting + change its save location + sanitizer integration
 /* Later TODO:
  * Maybe organize the tests better with TEST_F, INSTANTIATE_TEST_SUITE_P
  * Maybe create vectors with image configs
 */
-class Environment : public ::testing::Environment {
-    public:
-    ~Environment() override {}
 
-    void SetUp() override { }
+// Global configurations
+int worker_thread_count = 4;
+
+class MandelbrotTest : public ::testing::TestWithParam<float> {
+protected:
+    bool is_portrait;
+    
+    int (*color_map_red)(int, int) = sin_x_la_4;
+    int (*color_map_green)(int, int) = unu_minus_unu_pe_x;
+    int (*color_map_blue)(int, int) = x_patrat_0_1_to_0_2;
+
+    void SetUp() override {
+        is_portrait = false; // Default is landscape
+    }
 
     void TearDown() override {}
+
+    void generate_image(const std::string& name, float scale, float x_center, float y_center, 
+                        float zoom, int num_iters, int brightness, int rotate_degrees) {
+        if (is_portrait) {
+            mandelbrot_around_center(name.c_str(), 1080 * scale, 1080 * 1.2 * scale, x_center, y_center, 
+                                     zoom, num_iters, rotate_degrees, brightness, color_map_red, 
+                                     color_map_green, color_map_blue, mandelbrot_quadratic, worker_thread_count);
+        } else {
+            mandelbrot_around_center(name.c_str(), 1080 * scale, 1920 * scale, x_center, y_center, 
+                                     zoom, num_iters, rotate_degrees, brightness, color_map_red, 
+                                     color_map_green, color_map_blue, mandelbrot_quadratic, worker_thread_count);
+        }
+    }
 };
 
-// Mandelbrot
-TEST(image_generation, mandelbrot_1k) {
-    float scale = 1;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
+// Mandatory Mandelbrot Tests
+TEST_P(MandelbrotTest, mandelbrot_mandatory_tests) {
+    float scale = GetParam();
+    generate_image("test_image.ppm", scale, -0.43, -0.1, 1, 1500, 0, 0);
 }
 
-TEST(image_generation_extra, mandelbrot_1_5k) {
-    float scale = 1.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
+// Extra Mandelbrot Tests
+TEST_P(MandelbrotTest, mandelbrot_extra_tests) {
+    float scale = GetParam();
+    generate_image("test_image.ppm", scale, -0.43, -0.1, 1, 1500, 0, 0);
 }
 
-TEST(image_generation, mandelbrot_2k) {
-    float scale = 2;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
+// Galaxy Mandatory Tests
+TEST_P(MandelbrotTest, galaxy_mandatory_tests) {
+    is_portrait = true;
+    float scale = GetParam();
+    generate_image("test_image.ppm", scale, -0.700025 + 0.000000007, -0.26849991525, 
+                    0.0000000035, 1500, 0, 0);
 }
 
-TEST(image_generation_extra, mandelbrot_2_5k) {
-    float scale = 2.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
+// Galaxy Extra Tests
+TEST_P(MandelbrotTest, galaxy_extra_tests) {
+    is_portrait = true;
+    float scale = GetParam();
+    generate_image("test_image.ppm", scale, -0.700025 + 0.000000007, -0.26849991525, 
+                    0.0000000035, 1500, 0, 0);
 }
 
-TEST(image_generation_extra, mandelbrot_3k) {
-    float scale = 3;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
+// Stuff Mandatory Tests
+TEST_P(MandelbrotTest, stuff_mandatory_tests) {
+    is_portrait = true;
+    float scale = GetParam();
+    generate_image("test_image.ppm", scale, -0.7, -0.26, 0.01, 1500, 0, 1);
 }
 
-TEST(image_generation_extra, mandelbrot_3_5k) {
-    float scale = 3.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
+// Stuff Extra Tests
+TEST_P(MandelbrotTest, stuff_extra_tests) {
+    is_portrait = true;
+    float scale = GetParam();
+    generate_image("test_image.ppm", scale, -0.7, -0.26, 0.01, 1500, 0, 1);
 }
 
-TEST(image_generation, mandelbrot_4k) {
-    float scale = 4;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1920 * scale, -0.43, -0.1, 1, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
+// Seastar Mandatory Tests
+TEST_P(MandelbrotTest, seastar_mandatory_tests) {
+    is_portrait = true;
+    float scale = GetParam();
+    generate_image("test_image.ppm", scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1);
 }
 
-// Galaxy
-TEST(image_generation, galaxy_1k) {
-    float scale = 1;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
+// Seastar Extra Tests
+TEST_P(MandelbrotTest, seastar_extra_tests) {
+    is_portrait = true;
+    float scale = GetParam();
+    generate_image("test_image.ppm", scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1);
 }
 
-TEST(image_generation_extra, galaxy_1_5k) {
-    float scale = 1.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
+// Shells Mandatory Tests
+TEST_P(MandelbrotTest, shells_mandatory_tests) {
+    is_portrait = true;
+    float scale = GetParam();
+    generate_image("test_image.ppm", scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1);
 }
 
-TEST(image_generation, galaxy_2k) {
-    float scale = 2;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
+// Shells Extra Tests
+TEST_P(MandelbrotTest, shells_extra_tests) {
+    is_portrait = true;
+    float scale = GetParam();
+    generate_image("test_image.ppm", scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1);
 }
 
-TEST(image_generation_extra, galaxy_2_5k) {
-    float scale = 2.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
+INSTANTIATE_TEST_SUITE_P(
+    MandelbrotMandatoryTests,
+    MandelbrotTest,
+    ::testing::Values(1, 2, 4)
+);
 
-TEST(image_generation_extra, galaxy_3k) {
-    float scale = 3;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, galaxy_3_5k) {
-    float scale = 3.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation, galaxy_4k) {
-    float scale = 4;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.700025 + 0.000000007, -0.26849991525, 0.0000000035, 1500, 0, 0, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-// Stuff
-TEST(image_generation, stuff_1k) {
-    float scale = 1;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, stuff_1_5k) {
-    float scale = 1.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation, stuff_2k) {
-    float scale = 2;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, stuff_2_5k) {
-    float scale = 2.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, stuff_3k) {
-    float scale = 3;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, stuff_3_5k) {
-    float scale = 3.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation, stuff_4k) {
-    float scale = 4;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.7, -0.26, 0.01, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-// Seastar
-TEST(image_generation, seastar_1k) {
-    float scale = 1;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, seastar_1_5k) {
-    float scale = 1.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation, seastar_2k) {
-    float scale = 2;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, seastar_2_5k) {
-    float scale = 2.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, seastar_3k) {
-    float scale = 3;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, seastar_3_5k) {
-    float scale = 3.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation, seastar_4k) {
-    float scale = 4;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.72413, 0.28644, 0.0004, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-// Shells
-TEST(image_generation, shells_1k) {
-    float scale = 1;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, shells_1_5k) {
-    float scale = 1.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation, shells_2k) {
-    float scale = 2;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, shells_2_5k) {
-    float scale = 2.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, shells_3k) {
-    float scale = 3;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation_extra, shells_3_5k) {
-    float scale = 3.5;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
-
-TEST(image_generation, shells_4k) {
-    float scale = 4;
-    mandelbrot_around_center("test_image.ppm", 1080 * scale, 1080 * 1.2 * scale, -0.75 + 0.00005, -0.02, 0.00025, 1500, 0, 1, sin_x_la_4, unu_minus_unu_pe_x, x_patrat_0_1_to_0_2, mandelbrot_quadratic);
-}
+INSTANTIATE_TEST_SUITE_P(
+    MandelbrotExtraTests,
+    MandelbrotTest,
+    ::testing::Values(1.5, 2.5, 3, 3.5)
+);
